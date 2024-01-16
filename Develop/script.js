f4d43d02773c2d6ed75d3f65ecfeddaf968897b2
m13corona <mcorona1308@gmail.com>
2024-01-15 23:00:08 -0600

    modified javascript file, added lines of code

--- Develop/script.js
+++ Develop/script.js
@@ -9,25 +9,26 @@ var specialChar
 function generatePassword(){;
   insertLength = window.prompt("Enter password length (between 8 and 128 characters)");
 
-  if (insertLength < 8 || insertLength > 128 || isNaN(insertLength)) {;
-    window.alert("Please enter a length between 8 and 128 characters.");
-    
-
-  lowerCase = window.confirm("Do you want to include lowercase characters?");
-  upperCase = window.confirm("Do you want to include uppercase characters?");
-  numericChar = window.confirm("Do you want to include numeric characters?");
-  specialChar = window.confirm("Do you want to include special characters?");
-    
+  if (insertLength < 8 || insertLength > 128) {;
+    window.alert("Please enter a length between 8 and 128 characters.");  
 }
 
-var insertLength = Math.floor(Math.random() * options.length);
+lowerCase = window.confirm("Do you want to include lowercase characters?");
+upperCase = window.confirm("Do you want to include uppercase characters?");
+numericChar = window.confirm("Do you want to include numeric characters?");
+specialChar = window.confirm("Do you want to include special characters?");
+
+lowerCase = "abcdefghijklmnopqrstuvwxyz"
+upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numericChar = "1234567890"
+specialChar = "!?#$%._"
+
+var index = Math.floor(Math.random() * options.length);
+var computer = options[index];
+
 
 
 }
-
-
-
-
 
 
 
